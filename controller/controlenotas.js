1583dc7c7491c2d1f2aa67f13ceb0abec96e213c
Aelcio <aelciosrmacedo@gmail.com>
2021-06-06 19:21:59 -0300

    Desafio 31/05

--- controller/controlenotas.js
+++ controller/controlenotas.js
@@ -93,7 +93,7 @@ controller.save = async ({ usuarioId, titulo = null, descricao = null, checklist
         }
 
         notaSalva = { ...notaSalva, checklists: checklistsSalvos, tags: tagSalvas };
-
+        console.log(notaSalva)
         await transaction.commit();
 
         return notaSalva;
@@ -103,21 +103,112 @@ controller.save = async ({ usuarioId, titulo = null, descricao = null, checklist
 };
 
 //----------------------- Desafio PUT -------------------------------
-controller.edit = async (usuarioId, notaid, nota) => {
+// controller.edit = async (usuarioId, notaid, nota) => {
 
-    try {
-        controller.edit = async (usuarioId, id, { titulo = null, descricao = null, notas }) => {
+//     try {
+//         controller.edit = async (usuarioId, id, { titulo = null, descricao = null, notas }) => {
 
-            const notaAtu = await Nota.update({ titulo, descricao }, {
-                where: { usuarioId, id }
-            })
+//             const notaAtu = await Nota.update({ titulo, descricao }, {
+//                 where: { usuarioId, id }
+//             })
 
-        }
-    } catch (error) {
-        throw new (error);
-    }
-}
+//         }
+//     } catch (error) {
+//         throw new (error);
+//     }
+// }
 //--------------------------------------------------------------------
+
+controller.edit = async ({ usuarioId, titulo = null, descricao = null, checklists = [], tags = [] }, id) => {
+    const transaction = await sequelize.transaction();
+  
+    //try {
+      await Nota.update(
+        {
+          usuarioId,
+          titulo,
+          descricao,
+        },
+        {
+          where: { id },
+          transaction,
+        }
+      );
+  
+      let { dataValues } = await Nota.findOne({ where: { id }, transaction });
+      notaSalva = dataValues;
+  
+      let checklistsSalvos = [];
+  
+      if (checklists.length > 0) {
+        for (let checklist of checklists) {
+          checklist = { ...checklist, notaId: id };
+          let checklistSalvo;
+  
+          if (checklist.id) {
+            checklistSalvo = Object.assign({}, checklist);
+  
+            const checklistId = checklist.id;
+            delete checklist.id;
+  
+            await Checklist.update(checklist, {
+              where: {
+                id: checklistId,
+              },
+              transaction,
+            });
+          } else {
+            checklistSalvo = await Checklist.create(checklist, {
+              transaction,
+            });
+          }
+  
+          checklistsSalvos.push(checklistSalvo);
+        }
+      }
+  
+      let tagsSalvas = [];
+  
+      if (tags.length > 0) {
+        for (let tag of tags) {
+          tag = { ...tag, notaId: id };
+          let tagSalva;
+  
+          if (tag.id) {
+            tagSalva = Object.assign({}, tag);
+  
+            const tagId = tag.id;
+            delete tag.id;
+  
+            tagSalva = await Tag.update(tag, {
+              where: {
+                id: tagId,
+              },
+              transaction,
+            });
+          } else {
+            tagSalva = await Tag.create(tag, {
+              transaction,
+            });
+          }
+  
+          tagsSalvas = [...tagsSalvas, tagSalva];
+        }
+      }
+  
+      notaSalva = { ...notaSalva, checklists: checklistsSalvos, tags: tagsSalvas };
+  
+      console.log(notaSalva);
+  
+      await transaction.commit();
+  
+      return notaSalva;
+   /// } catch (error) {
+      //console.log(error);
+      //await transaction.rollback();
+    //}
+  };
+
 controller.remove = async (id) => {
     return await Nota.destroy({ where: { id } });
 }
